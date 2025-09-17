ce4c66e87cdc0d3aae8d34da4f46a51fd6b36a99
Kezia Paula De Oliveira <keziapaulaoliveira2345@gmail.com>
2025-09-17 16:27:15 -0300

    exerciciosdelupe.py

---
+++ Modulo 1/Loopings/01.tabuada.py
@@ -0,0 +1,17 @@
+# solicite ao usuario um numero inteiro de 1 a 10 e construa a tabuada deste numero
+
+# solicitaçao de entrada
+
+numero= int(input('digite um numero de 1 a 10: '))
+
+print(f"1 x {numero} = {1 * numero}")
+print(f"2 x {numero} = {1 * numero}")
+print(f"3 x {numero} = {1 * numero}")
+print(f"4 x {numero} = {1 * numero}")
+print(f"5 x {numero} = {1 *numero}")
+print(f"6 x {numero} = {1 * numero}")
+print(f"7 x {numero} = {1 * numero}")
+print(f"8 x {numero} = {1 * numero}")
+print(f"9 x {numero} = {1 * numero}")
+print(f"10 x {numero} = {1 * numero}")
+
---
+++ Modulo 1/Loopings/1.2.tabuada.py
@@ -0,0 +1,7 @@
+numero= int(input('digite um numero de 1 a 10: '))
+i= 1
+
+while i <= 10:
+    print(f" {i} x {numero} = {i * numero}")
+    i += 1 # i = i +1, quando i=1 => i=1  +1=27
+    
